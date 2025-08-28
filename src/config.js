79aa7115f6a1bba1009051651cd79479d98afdde
Jay <112299655+1552537028@users.noreply.github.com>
2025-08-28 16:06:33 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE ="https://nova-app-30ha.onrender.com" ||  "https://nova-docker-app.onrender.com/" ; 
+const API_BASE ="https://nova-app-30ha.onrender.com"; 
 export default API_BASE;
 //export const API_URL = `${API_BASE}/api`;
